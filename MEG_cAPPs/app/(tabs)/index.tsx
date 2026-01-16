2bc3f42f184d238029a6620a33298da08d17e72e
eugenisoler-meg <eugeni.soler@escoltesiguies.cat>
2026-01-16 13:39:22 +0100

    Connexió a la BD

--- MEG_cAPPs/app/(tabs)/index.tsx
+++ MEG_cAPPs/app/(tabs)/index.tsx
@@ -4,40 +4,26 @@ import * as SecureStore from 'expo-secure-store';
 import { Alert, Button, Platform, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  const [id, setId] = useState('');
-  const [date, setDate] = useState(new Date());
+  const [dni, setDNI] = useState('');
+  const [data_naixement, setDataNaixement] = useState(new Date());
   const [showPicker, setShowPicker] = useState(false);
 
   const login = async () => {
     try {
-      const res = await fetch("https://testapi.escoltesiguies.cat/auth/login", {
-        method: "POST",
+       const res = await fetch(`https://testapi.escoltesiguies.cat/login?dni=${dni}&data_naixement=${data_naixement.toISOString().split('T')[0]}`, {
+        method: "GET",
         headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({dni: "24414832X", birthdate: "2003-03-02"})
-      });
-
-      const text = await res.text();
-      console.log(text);
-      
-      const data = await res.json();
-
-      if (res.ok) {
-        await SecureStore.setItemAsync("token", data.token);
-        console.log("Logged in!", data.user);
-      } else {
-        alert(data.error);
-      }
-
-      // Extract JSON part
-      const jsonStart = text.indexOf('{');
-      const jsonText = text.slice(jsonStart);
-      const result = JSON.parse(jsonText);
-
-      if (result.status === 'success') {
-        Alert.alert('Success', 'Login successful');
-      } else {
-        Alert.alert('Error', 'Login failed');
-      }
+      }).then(response => response.text())
+      .then(async r => {
+          console.log("Response:", r);
+          // if(!r.error) {
+          //   await SecureStore.setItemAsync("token", r.token);
+          //   console.log("Logged in!", r.user);
+          // } else {
+          //   console.log("Error logging in:", r.error);
+          // }
+        }
+      );
     } catch (error: unknown) {
       console.log(error);
       if (error instanceof Error) {
@@ -52,8 +38,8 @@ export default function App() {
     <View style={{ padding: 20 }}>
       <Text>ID</Text>
       <TextInput
-        value={id}
-        onChangeText={setId}
+        value={dni}
+        onChangeText={setDNI}
         placeholder="Enter ID"
         style={{ borderWidth: 1, padding: 8, marginBottom: 10 }}
       />
@@ -63,18 +49,18 @@ export default function App() {
 
       {showPicker && (
         <DateTimePicker
-          value={date}
+          value={data_naixement}
           mode="date"
           display="default"
           onChange={(event, selectedDate) => {
             setShowPicker(Platform.OS === 'ios');
-            if (selectedDate) setDate(selectedDate);
+            if (selectedDate) setDataNaixement(selectedDate);
           }}
         />
       )}
 
       <Text style={{ marginVertical: 10 }}>
-        Selected date: {date.toISOString().split('T')[0]}
+        Selected date: {data_naixement.toISOString().split('T')[0]}
       </Text>
 
       <Button title="Login" onPress={login} />

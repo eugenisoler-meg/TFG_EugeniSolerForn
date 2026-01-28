cf1328f5374fbff81a194a5849cacfa5bb62a07d
eugenisoler-meg <eugeni.soler@escoltesiguies.cat>
2026-01-28 09:58:09 +0100

    File structure
    New file structure for a title and a login screen to an app.

--- MEG_cAPPs/app/login.tsx
+++ MEG_cAPPs/app/login.tsx
@@ -2,37 +2,44 @@ import DateTimePicker from '@react-native-community/datetimepicker';
 import { useState } from 'react';
 import * as SecureStore from 'expo-secure-store';
 import { Alert, Button, Platform, Text, TextInput, View } from 'react-native';
-import * as Utils from '../../constants/utils';
+import * as Utils from '../constants/utils';
 
 export default function App() {
   const [dni, setDNI] = useState('');
   const [data_naixement, setDataNaixement] = useState(new Date());
   const [showPicker, setShowPicker] = useState(false);
 
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<any>(null);
+  const [error, setError] = useState(false);
+
   const login = async () => {
+    setLoading(true);
+    setError(false);
+
     try {
-       const res = await fetch(`https://testapi.escoltesiguies.cat/login?dni=${dni}&data_naixement=${Utils.parseDate(data_naixement)}`, {
-        method: "GET",
-        headers: {"Content-Type": "application/json"},
-      }).then(response => response.json())
-      .then(async r => {
-          if(!r.error) {
-            Alert.alert("SESSIÓ INICIADA", `Benvingut/da ${JSON.parse(r.afiliat).nom + ' ' + JSON.parse(r.afiliat).cognoms}`);
-            await SecureStore.setItemAsync("USER", r.user);
-          } else {
-            console.log("Error logging in:", r.error);
-            Alert.alert("ERROR", JSON.parse(r.error).message);
-          }
-        }
-      );
-    } catch (error: unknown) {
-      console.log(error);
-      if (error instanceof Error) {
-        Alert.alert('ERROR', error.message);
+       const res = await fetch(`https://testapi.escoltesiguies.cat/login?dni=${dni}&data_naixement=${Utils.parseDate(data_naixement)}`, 
+       {method: "GET", headers: {"Content-Type": "application/json"},});
+      const json = await res.json();
+      setData(json);
+
+      if (json.error) throw new Error(json.error || 'Error during login');
+
+      await SecureStore.setItemAsync('USER', JSON.stringify(json.success.user));
+      Alert.alert('SUCCESS', 'Login successful');
+      
+    } catch (e) {
+      setError(true);
+      console.log(e);
+      if (e instanceof Error) {
+        Alert.alert('ERROR', e.message);
       } else {
         Alert.alert('ERROR', 'An unknown error occurred');
       }
+    } finally {
+      setLoading(false);
     }
+      
   };
 
   return (
